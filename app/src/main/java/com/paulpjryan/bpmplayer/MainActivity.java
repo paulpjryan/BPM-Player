9b0a79f0047baad07eff3fcf218bf8e3c5809b7c
paulpjryan <paulpjryan@gmail.com>
2015-10-14 17:59:55 -0400

    Play music from list.

--- app/src/main/java/com/paulpjryan/bpmplayer/MainActivity.java
+++ app/src/main/java/com/paulpjryan/bpmplayer/MainActivity.java
@@ -1,9 +1,14 @@
 package com.paulpjryan.bpmplayer;
 
+import android.content.ComponentName;
 import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -11,6 +16,7 @@ import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -20,6 +26,10 @@ public class MainActivity extends ActionBarActivity {
 
     private RecyclerView songView;
     private ArrayList<Song> songList;
+
+    private MusicService musicService;
+    private Intent playIntent;
+    private boolean musicBound = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +58,33 @@ public class MainActivity extends ActionBarActivity {
 
     }
 
+    private ServiceConnection musicConnection = new ServiceConnection(){
+
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            MusicService.MusicBinder binder = (MusicService.MusicBinder)service;
+            //get service
+            musicService = binder.getService();
+            //pass list
+            musicService.setList(songList);
+            musicBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            musicBound = false;
+        }
+    };
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if(playIntent == null) {
+            playIntent = new Intent(this, MusicService.class);
+            bindService(playIntent, musicConnection, Context.BIND_AUTO_CREATE);
+            startService(playIntent);
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -66,6 +103,11 @@ public class MainActivity extends ActionBarActivity {
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
+        } else if(id == R.id.action_stopmusic) {
+            stopService(playIntent);
+            musicService = null;
+            // TODO: make this not exit the app. Add regular music controls.
+            System.exit(0);
         }
 
         return super.onOptionsItemSelected(item);
@@ -75,7 +117,10 @@ public class MainActivity extends ActionBarActivity {
     public void getSongList() {
         ContentResolver musicResolver = getContentResolver();
         Uri musicUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
-        Cursor musicCursor = musicResolver.query(musicUri, null, null, null, null);
+
+        String selection = MediaStore.Audio.Media.IS_MUSIC + " != 0";
+
+        Cursor musicCursor = musicResolver.query(musicUri, null, selection, null, null);
 
         if(musicCursor != null && musicCursor.moveToFirst()) {
             int idColumn = musicCursor.getColumnIndex(MediaStore.Audio.Media._ID);
@@ -91,5 +136,11 @@ public class MainActivity extends ActionBarActivity {
         }
         if(musicCursor != null)
             musicCursor.close();
+    }
+
+    public void songPicked(View view) {
+        int pos = songView.getChildLayoutPosition(view);
+        musicService.setSong(pos);
+        musicService.playSong();
     }
 }
--- app/src/main/java/com/paulpjryan/bpmplayer/MusicService.java
+++ app/src/main/java/com/paulpjryan/bpmplayer/MusicService.java
@@ -1,16 +1,104 @@
 package com.paulpjryan.bpmplayer;
 
 import android.app.Service;
+import android.content.ContentUris;
 import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Binder;
 import android.os.IBinder;
+import android.os.PowerManager;
+import android.provider.MediaStore;
+import android.util.Log;
 
-public class MusicService extends Service {
+import java.util.ArrayList;
+
+public class MusicService extends Service implements
+    MediaPlayer.OnPreparedListener, MediaPlayer.OnErrorListener, MediaPlayer.OnCompletionListener {
+
+    private MediaPlayer player;     //media player
+    private ArrayList<Song> songs;  //songs
+    private int songPos;            //current song position
+
+    private final IBinder musicBind = new MusicBinder();
+
     public MusicService() {
+        //onCreate();
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        songPos = 0;
+        player = new MediaPlayer();
+        initMusicPlayer();
+    }
+
+    public void initMusicPlayer() {
+        //set player properties
+        player.setWakeMode(getApplicationContext(), PowerManager.PARTIAL_WAKE_LOCK);
+        player.setAudioStreamType(AudioManager.STREAM_MUSIC);
+
+        player.setOnPreparedListener(this);
+        player.setOnCompletionListener(this);
+        player.setOnErrorListener(this);
+    }
+
+    public void setList(ArrayList<Song> aSongs) {
+        songs = aSongs;
+    }
+
+    public class MusicBinder extends Binder {
+        MusicService getService() {
+            return MusicService.this;
+        }
+    }
+
+    public void playSong() {
+        //play a song
+        player.reset();
+        Song toPlay = songs.get(songPos);
+        long currSong = toPlay.getID();
+        Uri trackUri = ContentUris.withAppendedId(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, currSong);
+
+        try {
+            player.setDataSource(getApplicationContext(), trackUri);
+        } catch(Exception e) {
+            Log.e("MusicService", "Error setting data source", e);
+        }
+
+        player.prepareAsync();
+    }
+
+    public void setSong(int songIndex) {
+        songPos = songIndex;
     }
 
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return musicBind;
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        player.stop();
+        player.release();
+        return false;
+    }
+
+    @Override
+    public void onCompletion(MediaPlayer mediaPlayer) {
+
+    }
+
+    @Override
+    public boolean onError(MediaPlayer mediaPlayer, int i, int i2) {
+        return false;
+    }
+
+    @Override
+    public void onPrepared(MediaPlayer mediaPlayer) {
+        mediaPlayer.start();
     }
 }
